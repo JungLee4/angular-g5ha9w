d2a7553dde2525b6c1a737b752f2823069eedb1a
JungLee4 <jungunlee4@gmail.com>
2022-01-31 23:08:39 -0600

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -15,7 +15,7 @@ export class CartComponent implements OnInit {
     name: '',
     address: ''
   });
-  
+
   constructor(
     private cartService: CartService,
     private formBuilder: FormBuilder,
@@ -24,4 +24,11 @@ export class CartComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+  
 }
